d3e610115bc5ad985db59beeb58da484fa7ca402
JackHuynh <kietdien88@gmail.com>
2022-04-26 16:50:32 +0700

    feat(decompress): ignore package "decompress-tarxz"

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const decompressTar = require('decompress-tar');
 const decompressTarbz2 = require('decompress-tarbz2');
 const decompressTargz = require('decompress-targz');
 const decompressUnzip = require('decompress-unzip');
-const decompressTarXz = require('decompress-tarxz');
+// const decompressTarXz = require('decompress-tarxz');
 const kill = require('tree-kill');
 
 function fileExists(file) {
@@ -163,7 +163,7 @@ async function ensureBinary({ platform = process.platform, arch = process.arch, 
         decompressTarbz2(),
         decompressTargz(),
         decompressUnzip(),
-        decompressTarXz()
+        // decompressTarXz()
       ],
       filter: file => {
         return /(^|\/)speedtest(.exe)?$/.test(file.path);
