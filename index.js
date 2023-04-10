5e5c0cea3178a6963f5c69372719b2e96c37a393
Nhut Phan <thanquan@gmail.com>
2023-04-10 17:39:57 +0700

    update: remove decompress-tarxz

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ const decompressTar = require('decompress-tar');
 const decompressTarbz2 = require('decompress-tarbz2');
 const decompressTargz = require('decompress-targz');
 const decompressUnzip = require('decompress-unzip');
-// const decompressTarXz = require('decompress-tarxz');
 const kill = require('tree-kill');
 
 function fileExists(file) {
